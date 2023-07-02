0c72925fa10974535a705992974c19ffd7993517
juan pablo camargo T <juanpablocamargo91@gmail.com>
2023-07-02 17:35:07 -0500

    Lineal Regression, Basic Model

--- gd.py
+++ gd.py
@@ -4,15 +4,76 @@ import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 from plotter3D import MyPlot
+import math
 
-#importando tablas de datos
-temperature_df = pd.read_csv("./res/celsius_a_fahrenheit.csv")
 
-#cargando datos
-x = temperature_df['Celsius']
-y = temperature_df['Fahrenheit']
+def update_w_b(datos1, datos2, w, b, alpha):
+    dl_dw = 0.0  #se Inicializan las derivadas respecto a b y w
+    dl_db = 0.0
 
-#Visualizacion
-sns.scatterplot(x=x, y=y)
+    N = 0.0
+    if len(datos1) == len(datos2):
+        N = len(datos1)  #Se asigna el valor de N TODO: n = dato con menor len
+    else:
+        print('Error datos incompatibles')
 
-plt.show()
+    for i in range(N):
+        #La función A optimizar es la sumatoria de una función por ende sus derivadas parciales también son sumatorios de una función
+        dl_dw += -2 * datos1[i] * (datos2[i] - (w * datos1[i] + b))  #Derivada parcial de la función con respecto a A
+
+        dl_db += -2 * (datos2[i] - (w * datos1[i] + b))  #Derivada parcial de la función con respecto a B
+
+    #update w and b
+    w = w - (1 / N) * dl_dw * alpha
+    b = b - (1 / N) * dl_db * alpha
+
+    return w, b
+
+
+def avg_loss_MSE(datos1, datos2, w, b):
+    N = len(datos1)
+    total_error = 0.0
+    for i in range(N):
+        total_error += (datos2[i] - (w * datos1[i] + b))**2  #Función que se quiere optimizar
+    return total_error / float(N)
+
+
+def train(datos1, datos2, w, b, alpha, epoch):
+    grafica = MyPlot((datos1, datos2))
+    grafica.visualizationTrainProcess2D()
+
+    for e in range(epoch):
+        w, b = update_w_b(datos1, datos2, w, b, alpha)
+
+        #log progress
+        if e % (4**math.ceil(((5 * (e**2)) / (e**2 + 150 * e + 1)) - 0.3)) == 0 or e == epoch - 1:  #ignorar
+
+            print("epoch:", e, "loss: ", avg_loss_MSE(datos1, datos2, w, b))
+            grafica.updateLine2d(w, b)
+
+    grafica.keepOnScreen()
+
+    return w, b
+
+
+def main():
+    #importando tablas de datos
+    #temperature_df = pd.read_csv("./res/Salary Data.csv")
+
+    #cargando datos
+    #x = np.array(temperature_df['YearsExperience'])
+    #y = np.array(temperature_df['Salary'])
+
+    #importando tablas de datos
+    temperature_df = pd.read_csv("./res/celsius_a_fahrenheit.csv")
+
+    #cargando datos
+    x = np.array(temperature_df['Celsius'])
+    y = np.array(temperature_df['Fahrenheit'])
+
+    m, b = train(x, y, 0.0, 0.0, 0.0015, 15000)
+
+    print(m, b)
+
+
+main()
--- plotter3D.py
+++ plotter3D.py
@@ -3,17 +3,47 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import random
 
-class MyPlot:
-    def __init__(self, function):
-        self.function = function
-        plt.ion()
-        self.fig = plt.figure(figsize=(10, 10))
-        self.ax = plt.axes(projection='3d')
 
-    def plot(self):
-        x = y = np.arange(-6, 6.1, 0.2)
+def funPrueba(x, y=0):
+    return x**2 + y
+
+
+def StraightFunction(x, m, b):
+    return m * x + b
+
+
+class MyPlot:
+
+    def __init__(self, function=funPrueba, nprange=np.arange(-6, 6.1, 0.2)):
+        self.functionType = None
+        self.xRange = None
+        self.yRange = None
+        self.xFunction = None
+        self.yFunction = None
+
+        if type(function) == type((0, 0)):
+            self.xFunction = function[0]
+            self.yFunction = function[1]
+            self.functionType = 1  #funcion tipo nube de puntos
+            self.xRange = np.arange(self.xFunction[0], self.xFunction[-1], 0.2)
+        if callable(function):
+            self.function = function
+            self.xRange = nprange
+            self.yRange = nprange
+            self.functionType = 0  #funcion tipo funcion continua
+        else:
+            pass  #raise Exception('Function debe ser o una funcion (x,y) -> z o una tupla (X,Y) tipo np.array')
+
+        self.fig = plt.figure(figsize=(10, 9))
+
+        self.ax = None
+        self.line = None
+
+    def plot3D(self):
+        self.ax = plt.axes(projection='3d')
+        x = y = self.xRange
         X, Y = np.meshgrid(x, y)
-        ztemp = np.array(self.function(np.ravel(X),np.ravel(Y)))
+        ztemp = np.array(self.function(np.ravel(X), np.ravel(Y)))
         Z = ztemp.reshape(X.shape)
 
         surf = self.ax.plot_surface(X, Y, Z, cmap=plt.cm.cividis, zorder=2.2, alpha=0.5)
@@ -24,14 +54,18 @@ class MyPlot:
 
         self.fig.colorbar(surf, shrink=0.5, aspect=8)
 
+        point = None
         for _ in range(10):
-            # Updating the value of x and y
+            if point:
+                point.remove()  # Eliminar el punto anterior
+
+            # Updating the value of x, y, and z
             x = 0
-            y = random.randint(-5,5)
+            y = random.randint(-5, 5)
             z = 1
 
             # Plotting the new point
-            self.ax.scatter(x, y, z, color='red', s=50, zorder=2.5, alpha=1.0)
+            point = self.ax.scatter(x, y, z, color='red', s=50, zorder=2.5, alpha=1.0)
 
             # Re-drawing the figure
             self.fig.canvas.draw()
@@ -40,12 +74,46 @@ class MyPlot:
             self.fig.canvas.flush_events()
             plt.pause(0.25)
 
-            # Clearing the previous point
-            self.ax.collections[1].remove()
+        plt.show()
 
-def funPrueba(x,y):
-    return sum(x**2 + y)
+    def visualizationTrainProcess2D(self):
+        self.ax = plt.axes(xlabel='X', ylabel='Y', title='Gráfico')
 
-if __name__ == '__main__':    
-    grafica = MyPlot(funPrueba)
-    grafica.plot()
+        if self.functionType == 0:
+            x = self.xRange
+            y = np.array(self.function(x))
+            plt.plot(x, y)
+
+        if self.functionType == 1:
+            plt.scatter(self.xFunction, self.yFunction)
+
+    def updateLine2d(self, m, b):
+
+        if self.line:
+            self.line.remove()  # Eliminar la línea anterior
+
+        # Updating the value of x and y
+        yStraight = StraightFunction
+
+        # Plotting the new straight
+        self.line, = plt.plot(self.xRange, yStraight(self.xRange, m, b))
+
+        # Re-drawing the figure
+        self.fig.canvas.draw()
+
+        # To flush the GUI events
+        self.fig.canvas.flush_events()
+        plt.pause(0.25)
+
+    def keepOnScreen(self):
+        plt.show()
+
+
+if __name__ == '__main__':
+    datos = (np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]), np.array([1, 2, 3, 4, 5, 6, 7, 8, 9]))
+    print(type(datos))
+    grafica = MyPlot(datos)
+    grafica.visualizationTrainProcess2D()
+    for _ in range(10):
+        grafica.updateLine2d(random.randrange(-5, 5), -2)
+    grafica.keepOnScreen()
